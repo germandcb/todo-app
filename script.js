f61699ddf3a2836f0e13a977fedae76bd46aa5eb
German Celis <germancelis16@gmail.com>
2022-11-07 17:41:22 -0500

    clase final

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-( () => {
+import checkComplete from "./assets/components/checkComplete.js";
+import deleteIcon from "./assets/components/deleteIcon.js";
 const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
@@ -16,30 +17,12 @@ const createTask = (evento) => {
     titleTask.classList.add("task");
     titleTask.innerHTML = value;
     taskContent.appendChild(titleTask);
-    const content = `  
-     <div>
-    ${checkComplete()}
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
     //task.innerHTML = content;
     task.appendChild(taskContent);
-
+    task.appendChild(deleteIcon());
     list.appendChild(task);
 
 }
 
 btn.addEventListener('click', createTask);
 // listener 
-const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener("click", completeTask);
-    return i;
-}
-// Immediately invoked funtion expression IIFE
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle('fas');
-    element.classList.toggle('completeIcon');
-    element.classList.toggle('far');
-}
-})(); // Mandamos a llamar la funcion
